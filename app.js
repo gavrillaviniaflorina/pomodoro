fb385a06c143beecdffecb7c21ce04c31b3eba6d
lavinia gavril <lavinia.florina.gavril@gmail.com>
2022-03-02 16:50:42 +0200

    second commit

--- app.js
+++ app.js
@@ -1,63 +1,83 @@
-let timer = document.querySelector(".timer");
+let timeH = document.querySelector("h1");
+let hour = document.querySelector(".hour");
+let min = document.querySelector(".minute");
 let start = document.querySelector(".start");
-let buttons = document.querySelector(".buttons");
-let hour = document.querySelector("hour");
-let minute = document.querySelector(".minute");
-let time = new Date();
+let timeMinute = 70;
+let initial = timeMinute;
+let stopClicked = 0;
 
-hour.addEventListener("dblclick", (e) => {
 
-    timer.removeChild(hour);
-    let hours = document.createElement("input");
-    hours.classList = "hours";
-    timer.appendChild(hours);
+start.addEventListener("click", (e) => {
+
+    setInterval(timer, 1000);
+    CreateButtons();
 })
 
-start.addEventListener("click", handleStart);
 
-let handleStart = (e) => {
+displayTime(timeMinute);
+
+
+
+function timer() {
+    if (stopClicked == 0) {
+
+
+        timeMinute--;
+        displayTime(timeMinute);
+
+        if (timeMinute <= 0 || timeMinute < 1) {
+            clearInterval(countdown);
+        }
+    }
+
+}
+
+function displayTime(minutes) {
+    let hourValue = Math.floor(minutes / 60);
+    let minValue = Math.floor(minutes % 60);
+
+    if (hourValue < 10) {
+        hour.innerHTML = `0${hourValue}`;
+    } else {
+        hour.innerHTML = `${hourValue}`;
+    }
+
+    if (minValue < 10) {
+        min.innerHTML = `0${minValue}`;
+    } else {
+        min.innerHTML = `${minValue}`;
+    }
+
+
+}
+
+let CreateButtons = () => {
 
     let stop = document.createElement("BUTTON");
-    stop.classList = "stop";
-    stop.classList.add("but");
+    stop.classList = "but";
+    stop.classList.add("stop");
     stop.innerHTML = `Stop`;
+
     let reset = document.createElement("BUTTON");
-    reset.classList = "reset";
+    reset.classList = "but";
+    reset.classList.add("reset");
     reset.innerHTML = `reset`;
-    reset.classList.add("but");
-    buttons.removeChild(start);
-    buttons.appendChild(stop);
+
+    let buttons = document.querySelector(".buttons");
+    buttons.removeChild(buttons.firstElementChild);
+
+    buttons.appendChild(stop)
     buttons.appendChild(reset);
 
-}
+    stop.addEventListener("click", (e) => {
+        buttons.insertBefore(start, stop);
+        buttons.removeChild(stop);
+        stopClicked = 1;
+    })
 
 
-function getTime() {
+    reset.addEventListener("click", (e) => {
 
-
-    const hh = format(time.getHours());
-    const mm = format(time.getMinutes());
-    const ss = format(time.getSeconds());
-
-
-
-
-
-
-    return `${hh}:${mm}:${ss}`;
+    })
 
 }
-
-
-function format(number) {
-
-    if (number < 10) {
-        return "0" + number;
-    } else {
-        return number;
-    }
-}
-
-
-
-setInterval()
